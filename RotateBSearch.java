a558d0a3aae58eb233a720eb452694a6e3ada1ff
yw229 <yw229@njit.edu>
2012-11-02 16:02:18 -0300

    Update RotateBSearch.java

--- RotateBSearch.java
+++ RotateBSearch.java
@@ -1,4 +1,5 @@
-package Question9;
+//package Question9;
+/* Given a sorted array of n integers that has been rotated an unknown number of times, give an O(log n) algorithm that finds an element in the array. You may assume that the array was originally sorted in increasing order. */
 
 public class RotateBSearch {
 	public static int RotateSearch (int[] a, int low, int high, int x)
